4ddd1fa54b14d0cd68f17c2b40ed2f77d4856556
felipezingoni <felipezingoni.fz@gmail.com>
2023-08-01 16:06:46 -0300

    New project Recipes

---
+++ app/models/recipe.rb
@@ -0,0 +1,6 @@
+class Recipe < ApplicationRecord
+  validates :name, presence: true
+  validates :ingredients, presence: true
+  validates :instruction, presence: true
+  has_one_attached :image
+end
